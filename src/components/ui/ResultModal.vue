5a92bd1869beed3e47a50cdb2d825097a39a99fa
jlindhorst <lindhorst1990@gmail.com>
2026-07-10 09:45:53 +0200

    add new results modal and remove duplicated css and move to theme.css

---
+++ src/components/ui/ResultModal.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Modal from "@/components/ui/Modal.vue";
+
+defineProps<{ status: string, message: string }>()
+const emit = defineEmits<{ close: [] }>()
+</script>
+
+<template>
+  <modal
+      :class="status === 'success' ? 'success-modal' : 'fail-modal'"
+      :is-open="status !== 'pending'"
+      @close="emit('close')"
+  >
+    <p>{{ message }}</p>
+  </modal>
+</template>
